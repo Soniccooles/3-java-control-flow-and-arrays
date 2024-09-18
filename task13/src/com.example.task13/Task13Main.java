efefbb6e68577528201e6e571ac44da81a899585
Soniccooles <andrey.sonic@mail.ru>
2024-09-18 23:15:55 +0500

    Сакулин, модуль 3 - задание 13

--- task13/src/com.example.task13/Task13Main.java
+++ task13/src/com.example.task13/Task13Main.java
@@ -1,19 +1,12 @@
 package com.example.task13;
+import java.util.Arrays;
+public class Task13Main
+{
+    public static void main(String[] args) {}
 
-public class Task13Main {
-    public static void main(String[] args) {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        int[] arr = {9, 1100, 7, 8};
-        removeMoreThen1000(arr);
-        System.out.println(java.util.Arrays.toString(arr));
-         */
+    static int[] removeMoreThen1000(int[] arr)
+    {
+        if (arr == null) return null;
+        return Arrays.stream(arr).filter(number -> number <= 1000).toArray();
     }
-
-    static int[] removeMoreThen1000(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return  null;
-    }
-
 }
